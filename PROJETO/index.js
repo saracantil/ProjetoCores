b4dff3e5c776de7230b34650d4e021f72204bef5
saracantil <51168048+saracantil@users.noreply.github.com>
2021-10-27 20:48:47 -0300

    Update index.js

--- PROJETO/index.js
+++ PROJETO/index.js
@@ -1,7 +1,30 @@
-var button = document.getElementById('botao')
+const listaCores = ['green', '#FA67CD', '#777ABC',]
 
-    function quandoClicar(){
-        alert ("Botão Clicado")
+var button = document.getElementById('botao_cor')
+var input = document.getElementById('input_cor')
+var span = document.getElementById ('texto_cor')
+
+function quandoClicar (){
+    //alert(input.value)
+    var cor = input.value
+
+    if (input.value) {
+        // Tiver algo escrito (verdadeiro)
+    } else {
+        // se eu não tiver nada escrito (falso)
     }
+ 
     
-button.addEventListener('click', quandoClicar) 
+
+    
+
+    document.body.style.backgroundColor = cor
+    span.textContent = cor
+
+    // FORMAS PARA ESCREVER UMA COR
+    // cor em inglês: red, green, blue, yellow
+    // Hexadecimal: #8B008B, #FFFFFF
+    // rgb (139,0,29)
+    
+}
+button.addEventListener('click', quandoClicar)
