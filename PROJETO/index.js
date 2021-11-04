0604c32ae85a5d254b7a0d0f738334cd05e11bdb
saracantil <51168048+saracantil@users.noreply.github.com>
2021-11-03 21:53:35 -0300

    Update index.js

--- PROJETO/index.js
+++ PROJETO/index.js
@@ -1,30 +1,29 @@
-const listaCores = ['green', '#FA67CD', '#777ABC',]
+const listaCores = ['green','#FA67CD','#777ABC','rgb(200,45,0)']
 
-var button = document.getElementById('botao_cor')
+var button= document.getElementById('botao')
 var input = document.getElementById('input_cor')
-var span = document.getElementById ('texto_cor')
+var span = document.getElementById('texto_cor')
 
-function quandoClicar (){
-    //alert(input.value)
-    var cor = input.value
+function numeroRandomico(){
+    return Math.floor(Math.random() * listaCores.length)
+}
 
-    if (input.value) {
-        // Tiver algo escrito (verdadeiro)
-    } else {
-        // se eu não tiver nada escrito (falso)
+function quandoClicar(){
+    var cor 
+    
+    if(input.value){
+          cor= input.value
+    }else{
+        var randomico=numeroRandomico()
+        cor= listaCores[randomico]
     }
- 
-    
 
-    
-
-    document.body.style.backgroundColor = cor
+    document.body.style.backgroundColor= cor
     span.textContent = cor
 
-    // FORMAS PARA ESCREVER UMA COR
-    // cor em inglês: red, green, blue, yellow
-    // Hexadecimal: #8B008B, #FFFFFF
-    // rgb (139,0,29)
+}
+function numeroHex(){
     
 }
-button.addEventListener('click', quandoClicar)
+
+button.addEventListener('click',quandoClicar)
