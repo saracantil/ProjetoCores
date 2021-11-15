feb01fc35350684f66d9e60b73ac7a1e81c16d13
saracantil <51168048+saracantil@users.noreply.github.com>
2021-11-15 15:56:28 -0300

    Update hexa.js

--- PROJETO/hexa.js
+++ PROJETO/hexa.js
@@ -1,16 +1,26 @@
-const listaCores = ['green','#FA67CD','#777ABC','rgb(200,45,0)']
+const listaCores = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
 
-var button = document.getElementById('botao')
+var button= document.getElementById('botao')
+var span = document.getElementById('texto_cor')
+var cor
 
+function corRandomico(){
+     
+    var str = '#';
 
-function numeroRandomico(){
-    return Math.floor(Math.random() * listaCores.length)
+    while (str.length < 7) {
+        str+=Math.floor(Math.random() * listaCores.length)
+    }
+return str
 }
+
 
 function quandoClicar(){
-    
+
+    cor =corRandomico()
+
     document.body.style.backgroundColor= cor
     span.textContent = cor
+    }
 
-}
 button.addEventListener('click',quandoClicar)
